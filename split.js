3e874508b4d8096846ce718b348514acada514d7
Raellopes368 <raellopes368@gmail.com>
2020-11-19 16:28:02 -0300

    Diminui tamanho do arquivo

--- split.js
+++ split.js
@@ -23,7 +23,7 @@ async function readArq(arq) {
           config = true;
         }
       } else {
-        if (pages <= 50000) {
+        if (pages <= 30000) {
           archive = archive.concat(line).concat("\n");
           if (line.match(/\<\/page\>/)) {
             pages++;
